fa77dd457f98dc6cf4d6aa825aa7d2e98d2fd93c
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-01-25 19:33:22 -0600

    link API key/create getWeather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,29 @@
+ // Key storage variable
+ var apiKey = "91f9a95536d09a6da3e85f409255652c";
+ 
+ // Variables to store latitude/logitude values
+ var latitude = "32.77";
+ var longitude = "-96.80";
+
+ // function to fetch weather data
+ var getCityWeather = function(city) {
+    // format weather API
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+    // Response object - JSON formats the response
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+          });
+    })
+}
+getCityWeather();
+
+//variables to store a reference to the show repositories element
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 /*
-Step 1. Build the HTML/stylesheet for the dashboard with form inputs
-    - include: 
-        -search bar for the cities
-        -dashboard header
-        -jumbotron to display the city searched (city, date, temp, wind, humidity, uv index)
-        -5 day forcast with five widgets (date, weather icon, temp, wind, humidity)
-
-Step 2. link serverside api to javascript
 Step 3. Create a function to search for cities 
-            var getCity = function() {
-            console.log("function was called");};
-
-            getCity();
     - return city's current conditions, and 5 day forcast
             6.1.4
             If we assigned this returned object to a variable called response, - to access data:
@@ -36,6 +48,20 @@ Step 3. Create a function to search for cities
                 response.json().then(function(data) {    
                 console.log("inside", response);});});
 
+                Once we've captured the search input and used it to search for a City, 
+                we'll dynamically create more HTML and inject the list of repository data to 
+                the search history column.
+                    -add the search form (complete)
+                    -handle form submission
+                        Adding an event listener to the <form> element to execute a function on submission
+                            -add variables to store reference to form element (by id)
+                            -create a function for form submission
+                            -add even listener for form submittion
+                        Capturing the form's input data to use elsewhere in the app
+                            -filter the information that you would like to recieve/display
+                            -(When an api is called, it gives back more info than needed)
+                    -display response data on the page
+                    -add error handling
 
     - return UV index with colors for favorable, moderate, or severe (red, green, yellow?)
 Step 4. Create an area for storing previously searched cities (under the searchbar)
@@ -47,4 +73,33 @@ Resources
 https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
 https://coding-boot-camp.github.io/full-stack/
 https://docs.github.com/en/rest
+*/
+
+/*
+DONE Step 1: create funtion getCityWeather to fetch data from the 
+    weather API and return JSON data to console
+Step 2: create function formSubmitHandler to 
+    recieve data from form submission and return through get User Repos
+Step 3: create function displayRepos to display both the repos list and search term
+    then create a for loop within the funciton to diplay the results in html
+Step 4: add if statements to the getUserRepos function to 
+    - check the username is valid via Github's API (404 response)
+    - check if the user has repositories
+    - catch method to check for network errors
+*/
+
+/*
+Relavent information [daily 0 = today]
+City name (date: current.dt) (weather icon: current.weather.icon)
+temperature: current.temp
+wind: current.wind_speed?
+humidity: current.humidity
+uv index: current.uvi
+
+5 day forecast
+date: daily.dt [1, 2, 3, 4]
+icon: daily.weather.icon [1, 2, 3, 4]
+temp: daily.temp [1, 2, 3, 4]
+wind: daily.wind_speed [1, 2, 3, 4]
+humidity: daily.humidity [1, 2, 3, 4]
 */
