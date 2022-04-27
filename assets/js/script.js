919e44a3c39649c76b74ba47f2b3aef78189445c
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-27 00:44:08 -0500

    Refactors All

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-var apiKey = "c43c0f849cafd21471838779c2621b94";
-
-// City search form storage
+// City Search Form
 var citySearchEl = document.querySelector("#city-form");
 var cityNameEl = document.querySelector("#city-name");
 var searchButtonEl = document.querySelector("#city-storage");
 
-// Local storage variables
+// Local Storage
 var cityCount = 0;
 var searchHistory = [];
 
-// Input validator function
+// INPUT VALIDATOR
 var inputValidator = function (event) {
   event.preventDefault();
   var cityname = cityNameEl.value.trim();
-  // if the User input value to the search -> move to citySearchHandler function
+
+  // If Input is present, send a requst for lat/long values
   if (cityname) {
     citySearchHandler(cityname);
     cityNameEl.value = "";
@@ -22,49 +21,70 @@ var inputValidator = function (event) {
   }
 };
 
-// Function to search city by name for lat/long values
-var citySearchHandler = function () {
-  // Request to the open weather API for geolocation
-  var cityname = cityNameEl.value.trim();
+// CITY VALIDATOR
+var citySearchHandler = function (cityname) {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityname +
     "&appid=" +
     apiKey;
+
+  // Request to the Open Weather API for Geolocation
   fetch(apiUrl).then(function (response) {
-    // Condition: if Open weather API returns a valid response- use lat/lon in openWeatherRequest function
+    // If ok, lat/lon are valid
     if (response.ok) {
       response.json().then(function (location) {
-        // storing lat/long variables
+        // Store lat/long variables
         var latitude = location.coord.lat;
         var longitude = location.coord.lon;
 
-        // move to next functions (request weather data and save form input to a button)
+        // Request weather data, Save form input to a button
         openWeatherRequest(latitude, longitude, cityname);
         cityStorage(latitude, longitude, cityname);
       });
     }
-    // if there is no response from API based on user input - alert (invalid city)
+    // Else, Alert (invalid city)
     else {
       alert("City not found");
     }
   });
 };
 
-// Add buttons by search
+// REQUEST WEATHER DATA FROM OPEN WEATHER API
+var openWeatherRequest = function (latitude, longitude, cityname) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+
+  // Request for weather data using lat/long variables
+  fetch(apiUrl)
+    .then(function (response) {
+      // JSON formats the response under data
+      response.json().then(function (data) {
+        // If okay, display the weather
+        displayWeather(data, cityname);
+      });
+    })
+    // Else, inform the user- unable to connect to the API
+    .catch(function (error) {
+      console.log(error);
+      alert("Unable to connect to Open Source Weather");
+    });
+};
+
+// ADD SEARCH HISTORY BUTTONS
 var cityStorage = function (latitude, longitude, cityname) {
-  // create new button
+  // Create the button
   var newButtonEl = document.createElement("button");
-  // give the button a new #
   newButtonEl.setAttribute("id", cityCount);
-  // set the class
-  newButtonEl.setAttribute(
-    "class",
-    "btn btn-secondary rounded text-center text-dark col-lg-12 mb-4"
-  );
-  // save the name
+  newButtonEl.setAttribute("class", "btn previousSearch");
   newButtonEl.innerHTML = cityname;
-  // append to div
+  // Append
   document.getElementById("city-storage").appendChild(newButtonEl);
 
   // push variables into array for storage
@@ -82,56 +102,34 @@ var cityStorage = function (latitude, longitude, cityname) {
   cityCount++;
 };
 
-// function to grab values to run through openWEatherRequest
+// PREVIOUS SEARCH BUTTON HANDLER
 var reSearch = function (event) {
+  // Get the id
   var targetEl = event.target;
-  // get item #
   var i = targetEl.id;
+
+  // Grab the City data from localstorage
   var newSearch = localStorage.getItem("search");
   var cityData = JSON.parse(newSearch);
-  // grab city arrays from arrays
   var cityInfo = cityData[i];
-  // grab values to use from correct city array
+
+  // Define values from cityInfo array
   var cityname = cityInfo[0];
   var latitude = cityInfo[1];
   var longitude = cityInfo[2];
-  console.log("Displaying the weather for -", cityname, latitude, longitude);
+  // console.log("Displaying the weather for -", cityname, latitude, longitude);
 
-  // pass to openWeatherRequest function
+  // Request the Weather Data
   openWeatherRequest(latitude, longitude, cityname);
 };
 
-// Function to request weather data
-var openWeatherRequest = function (latitude, longitude, cityname) {
-  // request for weather data using lat/long variables
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    latitude +
-    "&lon=" +
-    longitude +
-    "&appid=" +
-    apiKey +
-    "&units=imperial";
-  // fetch the weather data (response)
-  fetch(apiUrl)
-    .then(function (response) {
-      // JSON formats the response under data
-      response.json().then(function (data) {
-        // move to displayWeather function and City storage?
-        displayWeather(data, cityname);
-        //console.log(data);
-      });
-    })
-    // notice: unable to connect to the API
-    .catch(function (error) {
-      console.log(error);
-      alert("Unable to connect to Open Source Weather");
-    });
-};
-
-// Function to dipsplay weather data
+// DISPLAY WEATHER DATA
 var displayWeather = function (weather, cityname) {
-  linebreak = "<br>";
+  // Use image constructor to add Open Weather Icon
+  var img = new Image();
+  var iconSource = weather.current.weather[0].icon;
+  img.src = "http://openweathermap.org/img/wn/" + iconSource + "@2x.png";
+  img.className = "currentIcon";
 
   // Format the date
   var unixUTCCurrent = weather.current.dt;
@@ -141,47 +139,36 @@ var displayWeather = function (weather, cityname) {
   // Update the Current Weather Card
   document.getElementById("cityName").innerHTML = cityname;
   document.getElementById("cityDate").innerHTML = date;
+  // Clear the old icon (if applicable), append new
+  document.getElementById("cityIcon").innerHTML = "";
+  document.getElementById("cityIcon").appendChild(img);
   document.getElementById("cityTemp").innerHTML = weather.current.temp + " °";
   document.getElementById("cityWeather").innerHTML =
     "Wind: " +
     weather.current.wind_speed +
-    "mph" +
-    linebreak +
-    "Humidity: " +
+    "mph <br> Humidity: " +
     weather.current.humidity +
-    "%" +
-    linebreak +
-    "UV Index: ";
+    "% <br> UV Index: ";
 
-  // function to create a span holding the UV index and colors
+  // Determine the UV index
   var uvi = weather.current.uvi;
-  var newSpan = document.createElement("span");
-  // if uvi is "2" or less - green
+  var uviEl = document.createElement("span");
   if (uvi <= 2) {
-    newSpan.setAttribute("class", "bg-success text-center");
-    // if uvi is between "3" to "5" - yellow
+    // If UVI is "2" or less - green
+    uviEl.setAttribute("class", "bg-success p-1 text-center");
+    // If UVI is between "3" to "5" - yellow
   } else if (uvi >= 3 && uvi <= 5) {
-    newSpan.setAttribute("class", "bg-warning text-center");
-    // if uvi is above "5" - red
+    uviEl.setAttribute("class", "bg-warning p-1 text-center");
+    // If UVI is above "5" - red
   } else {
-    newSpan.setAttribute("class", "bg-danger text-center");
+    uviEl.setAttribute("class", "bg-danger p-1 text-center");
   }
-  // display uvi value
-  newSpan.innerHTML = uvi;
-  // append to p element
-  document.getElementById("cityWeather").appendChild(newSpan);
+  // Set the #
+  uviEl.innerHTML = uvi;
+  // Append
+  document.getElementById("cityWeather").appendChild(uviEl);
 
-  // function to grab the image from Open Weather's Api to display
-  // uses image constructor
-  var img = new Image();
-  var iconSource = weather.current.weather[0].icon;
-  img.src = "http://openweathermap.org/img/wn/" + iconSource + "@2x.png";
-  img.className = "currentIcon";
-  // append to h2 element
-  document.getElementById("cityIcon").innerHTML = "";
-  document.getElementById("cityIcon").appendChild(img);
-
-  // Updates 5 Day Forecast widgets
+  // Update 5 Day Forecast cards
   for (let i = 0; i < 5; i++) {
     // Format the date
     var unixUTCdaily = weather.daily[i].dt;
@@ -195,21 +182,19 @@ var displayWeather = function (weather, cityname) {
 
     // Update each "day" card
     document.getElementById("cityDate" + i).innerHTML = dayDate;
+    // Clear the old icon (if applicable), append new
+    document.getElementById("cityIcon" + i).innerHTML = "";
+    document.getElementById("cityIcon" + i).appendChild(img);
     document.getElementById("cityTemp" + i).innerHTML =
       weather.daily[i].temp.day + " °";
     document.getElementById("cityWeather" + i).innerHTML =
       weather.daily[i].wind_speed +
-      "mph" +
-      linebreak +
-      +weather.daily[i].humidity +
+      "mph <br>" +
+      weather.daily[i].humidity +
       "%";
-    document.getElementById("cityIcon" + i).innerHTML = "";
-    document.getElementById("cityIcon" + i).appendChild(img);
   }
 };
 
-// event listener for Search button
+// Event Listeners
 citySearchEl.addEventListener("submit", inputValidator);
-
-// event listener for Search History clicks
 searchButtonEl.addEventListener("click", reSearch);
